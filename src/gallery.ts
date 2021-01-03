680bea26bed3d8128e94a41471f71c99bf8de511
Ash <ashp116@outlook.com>
2021-01-02 19:15:36 -0500

    added Gallery.ts file and created a gallery table

---
+++ src/gallery.ts
@@ -0,0 +1,16 @@
+import { v4 as uuid } from 'uuid';
+import { Request, Response } from 'express';
+import { DB } from './db';
+import { User, EDITOR_ROLE } from './User';
+
+export class Gallery {
+    private sql: DB;
+
+    constructor(db: DB) {
+        this.sql = db;
+    }
+
+    public createAlbum() {
+        this.sql.query("SELECT * FROM")
+    }
+}
